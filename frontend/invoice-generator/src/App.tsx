b558daa2f26ab5b46c12d1424e4231667b473627
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-10-03 22:58:22 +0530

    feat: Implement routing and layout for the application

--- frontend/invoice-generator/src/App.tsx
+++ frontend/invoice-generator/src/App.tsx
@@ -1,8 +1,57 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+import { Toaster } from "react-hot-toast";
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import ProfilePage from "./pages/Profile/ProfilePage";
+import AllInvoices from "./pages/Invoices/AllInvoices";
+import CreateInvoice from "./pages/Invoices/CreateInvoice";
+import InvoiceDetail from "./pages/Invoices/InvoiceDetail";
+import LandingPage from "./pages/LandingPage/LandingPage";
+
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+
 function App() {
   return (
-    <>
-      <h1 className="text-center text-4xl">App</h1>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected routes */}
+
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/invoices" element={<AllInvoices />} />
+            <Route path="/invoices/new" element={<CreateInvoice />} />
+            <Route path="/invoies/:id" element={<InvoiceDetail />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
+
+          {/* Catch all route */}
+          <Route path="*" element={<Navigate to={"/"} replace />} />
+        </Routes>
+      </Router>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px"
+          }
+        }}
+      />
+    </div>
   )
 }
 
