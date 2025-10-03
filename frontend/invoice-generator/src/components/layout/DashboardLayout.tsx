8feecf96a9838606a1d601c329fd50bb3d857cd5
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-10-03 22:59:20 +0530

    feat: Create DashboardLayout component

---
+++ frontend/invoice-generator/src/components/layout/DashboardLayout.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  children?: React.ReactNode
+};
+
+const DashboardLayout = ({ children }: Props) => {
+  return (
+    <div>
+      DashboardLayout
+      {children}
+    </div>
+  )
+}
+
+export default DashboardLayout
