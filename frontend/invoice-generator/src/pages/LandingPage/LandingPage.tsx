694ebfe15825b0c933ec5bea299a9b0295ef25f8
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-10-04 23:03:57 +0530

    feat: Add Hero component to LandingPage

--- frontend/invoice-generator/src/pages/LandingPage/LandingPage.tsx
+++ frontend/invoice-generator/src/pages/LandingPage/LandingPage.tsx
@@ -1,9 +1,13 @@
 import Header from "../../components/landing/Header"
+import Hero from "../../components/landing/Hero"
 
 const LandingPage = () => {
   return (
     <div className="text-gray-600 bg-[#ffffff]">
       <Header />
+      <main>
+        <Hero />
+      </main>
     </div>
   )
 }
