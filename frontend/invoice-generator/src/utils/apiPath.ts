2745a96d0c46783abe22cb848cf3c5e68e165097
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-10-16 23:00:58 +0530

    feat: Define API paths for authentication, invoices, and AI.

---
+++ frontend/invoice-generator/src/utils/apiPath.ts
@@ -0,0 +1,25 @@
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "http://localhost:8000/api";
+
+export const API_PATHS = {
+	AUTH: {
+		REGISTER: "/auth/register", // Signup
+		LOGIN: "/auth/login", // Authenticate user & return JWT token
+		GET_PROFILE: "/auth/me", // (GET) Get logged-in user details
+		UPDATE_PROFILE: "/auth/me", // (PUT) update profile details
+	},
+
+	INVOICE: {
+		CREATE: "/invoices/", // (POST)
+		GET_ALL_INVOICES: "/invoices/",
+		GET_INVOICE_BY_ID: (id: string) => `/invoices/${id}`,
+		UPDATE_INVOICE: (id: string) => `/invoices/${id}`,
+		DELETE_INVOICE: (id: string) => `/invoices/${id}`,
+	},
+
+	AI: {
+		PARSE_INVOICE_TEXT: "/ai/parse-text",
+		GENERATE_REMINDER: "/ai/generate-reminder",
+		GET_DASHBOARD_SUMMARY: "/ai/dashboard-summary",
+	},
+};
