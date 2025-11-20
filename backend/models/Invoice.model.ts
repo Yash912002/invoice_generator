8a9d0b31b62fc7842423a956e4c03bd9b278a9c7
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-11-20 20:44:23 +0530

    refactor: Update the type of items from Array<IItem> to IItem[]

--- backend/models/Invoice.model.ts
+++ backend/models/Invoice.model.ts
@@ -1,102 +1,113 @@
 import { Document, model, Schema } from "mongoose";
+import type { IUser } from "./User.model.js";
 
 export interface IItem extends Document {
-	name: string;
-	quantity: number;
-	unitPrice: number;
-	taxPercent: number;
-	total: number;
+  name: string;
+  quantity: number;
+  unitPrice: number;
+  taxPercent: number;
+  total: number;
 }
 
 const itemSchema = new Schema<IItem>({
-	name: {
-		type: String,
-		required: true,
-	},
-	quantity: {
-		type: Number,
-		required: true,
-	},
-	unitPrice: {
-		type: Number,
-		required: true,
-	},
-	taxPercent: {
-		type: Number,
-		default: 0,
-	},
-	total: {
-		type: Number,
-		required: true,
-	},
+  name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  unitPrice: {
+    type: Number,
+    required: true,
+  },
+  taxPercent: {
+    type: Number,
+    default: 0,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
 });
 
 export interface IInvoice extends Document {
-	user: Schema.Types.ObjectId;
-	invoiceNumber: string;
-	invoiceDate: Date;
-	dueDate: Date;
-	billFrom: {};
-	billTo: {};
-	items: Array<IItem>;
-	notes: string;
-	paymentTerms: string;
-	status: string;
-	subtotal: number;
-	taxTotal: number;
-	total: number;
-	createdAt: Date;
-	updatedAt: Date;
+  user: IUser;
+  invoiceNumber: string;
+  invoiceDate: Date;
+  dueDate: Date;
+  billFrom: {
+    businessName: string;
+    email: string;
+    address: string;
+    phone: string;
+  };
+  billTo: {
+    clientName: string;
+    email: string;
+    address: string;
+    phone: string;
+  };
+  items: IItem[];
+  notes?: string;
+  paymentTerms: string;
+  status: string;
+  subtotal: number;
+  taxTotal: number;
+  total: number;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 const invoiceSchema = new Schema<IInvoice>(
-	{
-		user: {
-			type: Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-		invoiceNumber: {
-			type: String,
-			required: true,
-		},
-		invoiceDate: {
-			type: Date,
-			default: Date.now,
-		},
-		dueDate: {
-			type: Date,
-		},
-		billFrom: {
-			businessName: String,
-			email: String,
-			address: String,
-			phone: String,
-		},
-		billTo: {
-			clientName: String,
-			email: String,
-			address: String,
-			phone: String,
-		},
-		items: [itemSchema],
-		notes: {
-			type: String,
-		},
-		paymentTerms: {
-			type: String,
-			default: "Net 15",
-		},
-		status: {
-			type: String,
-			enum: ["Paid", "Unpaid"],
-			default: "Unpaid",
-		},
-		subtotal: Number,
-		taxTotal: Number,
-		total: Number,
-	},
-	{ timestamps: true }
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    invoiceNumber: {
+      type: String,
+      required: true,
+    },
+    invoiceDate: {
+      type: Date,
+      default: Date.now,
+    },
+    dueDate: {
+      type: Date,
+    },
+    billFrom: {
+      businessName: String,
+      email: String,
+      address: String,
+      phone: String,
+    },
+    billTo: {
+      clientName: String,
+      email: String,
+      address: String,
+      phone: String,
+    },
+    items: [itemSchema],
+    notes: {
+      type: String,
+    },
+    paymentTerms: {
+      type: String,
+      default: "Net 15",
+    },
+    status: {
+      type: String,
+      enum: ["Paid", "Unpaid"],
+      default: "Unpaid",
+    },
+    subtotal: Number,
+    taxTotal: Number,
+    total: Number,
+  },
+  { timestamps: true },
 );
 
 export default model<IInvoice>("Invoice", invoiceSchema);
