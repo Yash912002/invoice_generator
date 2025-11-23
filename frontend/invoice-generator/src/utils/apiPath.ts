1e19fb01f11e53e2852698ca72e7d287614a725b
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-11-23 20:52:45 +0530

    chore: Use VITE_BASE_URL instead of hard-coded API URL

--- frontend/invoice-generator/src/utils/apiPath.ts
+++ frontend/invoice-generator/src/utils/apiPath.ts
@@ -1,5 +1,4 @@
-// export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const API_PATHS = {
 	AUTH: {
